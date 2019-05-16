5873af1d30ce71828c568b4d94bfd027395ef545
Jake Tayler <thainav2010@gmail.com>
2019-05-16 00:57:24 -0300

    Adição da pagina de envio de email para contato.

--- send.php
+++ send.php
@@ -1,10 +1,30 @@
 <?php
-//variaveis do dados capturador no formulario de contato.
-$email; 
-$name;
-$subject;
-$msg;
+ 
+ if(isset($_POST['email']) && !empty($_POST['email'])){
 
+    //variaveis do dados capturador no formulario de contato.
+    $email = $_POST['email']; 
+    $name = $_POST['name'];
+    $msg = $_POST['msg'];
+ 
+    $to = "contato@studioths.net";
+    $subject ="Orçamento - THS Studio";
+    $body = "Nome:".$name. "\r\n".
+            "Email:".$email. "\r\n".
+            "Mensagem:". $msg;
+    $header = "From:contato@studioths.net"."\r\n"
+            ."Replay-to".$email. "\r\n"
+            ."X=Mailer:PHP/".phpversion();
+    
+    if(mail($to,$subject,$body,$header)){
+        echo(" Mensagem enviado com sucesso.");
+    }else{
+        echo("Email não enviado.");
+    }
+
+
+
+ }
 
 
 
