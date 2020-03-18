b953ed308780b8023243153aeb8fdf11231e1819
Jake Tayler <thainav2010@gmail.com>
2020-03-18 17:26:59 -0300

    Update assets, the project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require ('browser-sync').create();
-
+const minifyCSS = require('gulp-minify-css');
 
 function style(){
     return gulp.src('./sass/*.scss')
@@ -9,18 +9,22 @@ function style(){
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
 }
-
+function miniCSS(){
+    return gulp.src('./css/*.css')
+    .pipe(minifyCSS({keepBreaks:false}))
+    .pipe(gulp.dest('./css/*.min.css'));
+}
 function watch(){
     browserSync.init({
         server:{
             baserDir:'./'
         }
     });
-    gulp.watch('./scss/*.scsss', style);
+    gulp.watch('./scss/*.scsss', style).on('change',browserSync.reload);
     gulp.watch('./*.html').on('change',browserSync.reload);
     gulp.watch('./js/*.js').on('change',browserSync.reload);
 }
 
-const defaultTaskt = gulp.parallel(style, watch)
+const defaultTaskt = gulp.parallel(style,watch)
 
 exports.default = defaultTaskt;
