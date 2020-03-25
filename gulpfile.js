7608d9ffdd8c9e9a54453317c2204a267b48ea1b
Jake Tayler <thainav2010@gmail.com>
2020-03-25 12:08:22 -0300

    Add new feature for move file js

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,8 @@ function watch(){
         }
     });
     gulp.watch('./scss/*.scsss', style);
+    gulp.watch('./css/*.css').on('change',browserSync.reload);
+    gulp.watch('./scss/*.scss').on('change',browserSync.reload);
     gulp.watch('./*.html').on('change',browserSync.reload);
     gulp.watch('./js/*.js').on('change',browserSync.reload);
 }
