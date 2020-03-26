9106e6aeb256b4e62e629eeb9a4c6d5e73ffca9a
Jake Tayler <thainav2010@gmail.com>
2020-03-26 16:13:28 -0300

    System for send emails.

--- send.php
+++ send.php
@@ -5,27 +5,26 @@
     //variaveis do dados capturador no formulario de contato.
     $email = $_POST['email']; 
     $name = $_POST['name'];
+    $assunto = $_POST['assunto'];
     $msg = $_POST['msg'];
  
     $to = "contato@studioths.net";
-    $subject ="Orçamento - THS Studio";
+    $subject ="Orçamento para ".$assunto." -THS Studio";
     $body = "Nome:".$name. "\r\n".
             "Email:".$email. "\r\n".
             "Mensagem:". $msg;
-    $header = "From:contato@studioths.net"."\r\n"
+    $header = "From:thainav2010@gmail.com"."\r\n"
             ."Replay-to".$email. "\r\n"
             ."X=Mailer:PHP/".phpversion();
     
     if(mail($to,$subject,$body,$header)){
         echo(" Mensagem enviado com sucesso.");
+        header('Location: ./contato.html?sucesso');
     }else{
         echo("Email não enviado.");
+        header('Location: ./contato.html?erro');
     }
-
-
-
  }
-
 
 
 ?>
